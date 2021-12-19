b66026df6f8f7fc171f0b569b5a7c1c0093f60a4
Ben Fowler <bf118@canterbury.ac.uk>
2021-12-19 15:44:55 +0000

    Created bot in c# rather than js

---
+++ bot/bot/Modules/Commands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using System.IO;
+using Discord;
+using Discord.WebSocket;
+
+namespace bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello")]
+        public async Task Hello()
+        {
+            await ReplyAsync("Hello there");
+        }
+    }
+
+}
