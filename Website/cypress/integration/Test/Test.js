b936b18b389ab006c00d0136169857ec248a08a0
Ben Fowler <bf118@canterbury.ac.uk>
2022-01-04 10:56:51 +0000

    Added Cypress tests

---
+++ Website/cypress/integration/Test/Test.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+///https://on.cypress.io/introduction-to-cypress
+
+describe('Final Year Project', () => {
+    beforeEach(() => {
+        cy.visit('http://bf118.webhosting.canterbury.ac.uk/')
+    })
+
+    it('can see four encounter Buttons', () => {
+
+        cy.get('button').contains('Encounter1')
+        cy.get('button').contains('Encounter2')
+        cy.get('button').contains('Encounter3')
+        cy.get('button').contains('Encounter4')
+    })
+
+    it('Clicks encounter 1 and 4 buttons', () => {
+        cy.get('button').contains('Encounter1').click()
+        cy.get('button').contains('Encounter4').click()
+    })
+
+    it('Create signup updates sheet', () => {
+
+
+        cy.get('textarea').should('have.value', 'Test Sheet')
+        cy.get('button').contains('Create Signup').click()
+
+        cy.get('textarea').should('have.value', 'Updated')
+    })
+})
