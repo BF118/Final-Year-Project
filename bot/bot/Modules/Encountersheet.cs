29f9cfad69c0ba565da78914847c56520c9d981d
Ben Fowler <bf118@canterbury.ac.uk>
2022-02-19 15:18:03 +0000

    encounter sheet updates

--- bot/bot/Modules/Encountersheet.cs
+++ bot/bot/Modules/Encountersheet.cs
@@ -17,6 +17,7 @@ namespace bot.Modules
         public class Commands : ModuleBase<SocketCommandContext>
         {
             public Optional<IUser> User { get; }
+          
             [Command("encounter1")]
             public async Task testEncounter()
             {
@@ -25,12 +26,12 @@ namespace bot.Modules
                 var healer = new Emoji("❤️");
                 var learner = new Emoji("🎓");
 
-                var loading = Path.GetFileName("E:/GITHUB_UNI/fyp/bot/bot/Modules/assets/Loading.gif");
+                var loading = Path.GetFileName("/GITHUB_UNI/fyp/bot/bot/Modules/assets/Loading.gif");
 
                 EmbedBuilder Encounter1 = new EmbedBuilder()
 
                 .WithTitle("Loading....")
-                .AddField("Encounter 1 loading","")
+                .AddField("Encounter 1 loading","....")
                 .WithImageUrl($"attachment://{loading}")
                 .AddField("Role", MentionUtils.MentionRole(933000592362725396))
                 .WithCurrentTimestamp()
@@ -43,8 +44,11 @@ namespace bot.Modules
                 await sent.AddReactionAsync(healer);
                 await sent.AddReactionAsync(learner);
 
-                var roleencounter1 = Context.Message.GetReactionUsersAsync(shield, 100, null) as SocketGuildUser;
 
+                var myid = 380426938432618496;
+                var roleencounter1 = Context.Message.GetReactionUsersAsync(shield, 100, null) as SocketGuildUser;
+                var role1 = Convert.ToUInt64(roleencounter1);
+                var role2 = Convert.ToUInt64(myid);
 
 
                 await (sent).ModifyAsync(x =>
@@ -56,7 +60,7 @@ namespace bot.Modules
                     .AddField("Looting:", " test")
                     .AddField("Team size:", " 0/7")
                     .AddField("Reactions:", "Remove your signup and role")
-                    .AddField("<:shield:927174765058326558> 1x Base Tank:", (Context.User.Mention))
+                    .AddField("<:shield:927174765058326558> 1x Base Tank:", MentionUtils.MentionUser(role1))
                     .AddField("<:crossed_swords:927174860524896276> 4x dps:", " .", true)
                     .AddField("<:heart:927185322050199612> 1x Healer", ". ", true)
                     .AddField("<:mortar_board:927185690867937330> 1x Learner:", ".", true)
@@ -66,24 +70,33 @@ namespace bot.Modules
                      x.Embed = encounter_edit.Build();
                  });
 
-                await (sent).ModifyAsync(x =>
+                if((roleencounter1 is null))
                 {
-                    EmbedBuilder encounter_edit = new EmbedBuilder()
-                   .WithTitle("Test Encounter")
-                   .AddField("Time:", " n/a")
-                   .AddField("Looting:", " test")
-                   .AddField("Team size:", " 0/7")
-                   .AddField("Reactions:", "Remove your signup and role")
-                   .AddField("<:shield:927174765058326558> 1x Base Tank:", (Context.User.Mention))
-                   .AddField("<:crossed_swords:927174860524896276> 4x dps:", " .", true)
-                   .AddField("<:heart:927185322050199612> 1x Healer", ". ", true)
-                   .AddField("<:mortar_board:927185690867937330> 1x Learner:", ".", true)
-                   .AddField("Role", MentionUtils.MentionRole(933000592362725396))
-                   .WithCurrentTimestamp()
-                   .WithColor(Color.Red);
-                    x.Embed = encounter_edit.Build();
+                    await (sent).ModifyAsync(x =>
+                    {
+                        EmbedBuilder encounter_edit = new EmbedBuilder()
 
-                });
+                       .WithTitle("Test Encounter")
+                       .AddField("Time:", " n/a")
+                       .AddField("Looting:", " test")
+                       .AddField("Team size:", " 0/7")
+                       .AddField("Reactions:", "Remove your signup and role")
+                       .AddField("<:shield:927174765058326558> 1x Base Tank:", MentionUtils.MentionUser(role1))
+                       .AddField("<:crossed_swords:927174860524896276> 4x dps:", MentionUtils.MentionUser(role2), true)
+                       .AddField("<:heart:927185322050199612> 1x Healer", MentionUtils.MentionUser(role1), true)
+                       .AddField("<:mortar_board:927185690867937330> 1x Learner:", MentionUtils.MentionUser(role1), true)
+                       .AddField("Role", MentionUtils.MentionRole(933000592362725396))
+                       .WithCurrentTimestamp()
+                       .WithColor(Color.DarkRed);
+                        x.Embed = encounter_edit.Build();
+                    });
+
+
+
+
+
+
+                }
             }
 
         }
