a072d3f94c240c1a71d6d6b3c8a59fb14626bc7b
Ben Fowler <bf118@canterbury.ac.uk>
2022-02-27 16:34:37 +0000

    <mod>Test.js, index.html, styles.css
    Added tests for checking username is added to sign up, that the modals are working, can see when someone signs up
    website and styles updated to what is on the server

--- Website/cypress/integration/Test/Test.js
+++ Website/cypress/integration/Test/Test.js
@@ -18,16 +18,31 @@ describe('Final Year Project', () => {
         cy.get('button').contains('Encounter1').click()
         cy.get('button').contains('Encounter4').click()
     })
-
     it('Create signup updates sheet', () => {
 
+       cy.get('button').contains('Create Signup').click()
 
-        cy.get('textarea').should('have.value', 'Test Sheet')
-        cy.get('button').contains('Create Signup').click()
-
-        cy.get('textarea').should('have.value', 'Updated')
+    
     })
     it('Open Discord invite', () => {
         cy.get('[alt="Discord"]').invoke('removeAttr', 'target').click()
+    })
+    it('Add username to role sign up', () => {
+        cy.get('input[name=username]').type('testuser')
+        cy.get('button').contains('Click here to sign up').click()
+        cy.get('input[name=baseTank]').should('have.value', 'testuser')
+    })
+    it('Check role information', () => {
+        cy.get('[alt=baseTank]').invoke('removeAttr', 'target').click()
+        cy.get('p').contains('I am the base')
+    })
+    it('Can close modal', () => {
+        cy.get('[alt=baseTank]').invoke('removeAttr', 'target').click()
+        cy.get('[class="popup__close"]').contains('Press here to Close').click()
+    })
+    it('Can count number of roles assigned', () => {
+        cy.get('input[name=username]').type('testuser')
+        cy.get('button').contains('Click here to sign up').click()
+        cy.get('p').should('have.value', '')
     })
 })
